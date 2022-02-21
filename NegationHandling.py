7465596c742119eda3ee94330c32f2bab13d72e5
Utkarsh_lal <utkarshredd@gmail.com>
2022-02-21 18:31:16 +0530

    Main negation handler

---
+++ NegationHandling.py
@@ -0,0 +1,30 @@
+def negation_handler(sentence):	
+    temp = int(0)
+    for i in range(len(sentence)):
+        if sentence[i-1] in ['not',"n't"]:
+            antonyms = []
+            for syn in wordnet.synsets(sentence[i]):
+                syns = wordnet.synsets(sentence[i])
+                w1 = syns[0].name()
+                temp = 0
+                for l in syn.lemmas():
+                    if l.antonyms():
+                        antonyms.append(l.antonyms()[0].name())
+                max_dissimilarity = 0
+                for ant in antonyms:
+                    syns = wordnet.synsets(ant)
+                    w2 = syns[0].name()
+                    syns = wordnet.synsets(sentence[i])
+                    w1 = syns[0].name()
+                    word1 = wordnet.synset(w1)
+                    word2 = wordnet.synset(w2)
+                    if isinstance(word1.wup_similarity(word2), float) or isinstance(word1.wup_similarity(word2), int):
+                        temp = 1 - word1.wup_similarity(word2)
+                    if temp>max_dissimilarity:
+                        max_dissimilarity = temp
+                        antonym_max = ant
+                        sentence[i] = antonym_max
+                        sentence[i-1] = ''
+    while '' in sentence:
+        sentence.remove('')
+    return sentence
